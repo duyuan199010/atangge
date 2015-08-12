4424303e0688c5575e5f448a7c08ab31a410d435
lying <lai_ying@163.com>
2015-08-12 20:12:41 +0800

    add

---
+++ yssl/src/com/strod/yssl/clientcore/HttpRequestId.java
@@ -0,0 +1,13 @@
+package com.strod.yssl.clientcore;
+/**
+ * http请求编号，用于客户端标识不同的请求
+ * @author user
+ *
+ */
+public interface HttpRequestId {
+
+	/**内容列表刷新*/
+	public static final int CONTENT_LIST_REFRESH = 0x0001;
+	/**内容列表加载更多*/
+	public static final int CONTENT_LIST_LOADMORE = 0x0002;
+}
