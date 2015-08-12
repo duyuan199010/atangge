789ca0eb314514bd607aef050a7e3c1d47f7123b
laiying <laiying@9zhitx.com>
2015-08-12 19:00:54 +0800

    library update

--- yssl/src/com/strod/yssl/clientcore/Config.java
+++ yssl/src/com/strod/yssl/clientcore/Config.java
@@ -2,7 +2,6 @@ package com.strod.yssl.clientcore;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -10,12 +9,10 @@ import java.security.NoSuchAlgorithmException;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Environment;
 
 import com.roid.config.AbsConfig;
-import com.nostra13.universalimageloader.cache.disc.impl.ext.DiskLruCache;
+import com.strod.yssl.clientcore.cache.DiskLruCache;
 
 public class Config extends AbsConfig {
 
@@ -36,11 +33,8 @@ public class Config extends AbsConfig {
 
 	/** image cache dir name */
 	// http://blog.csdn.net/guolin_blog/article/details/28863651
-	private static final String IMAGE_CACHE_DIR = "bitmap";
-	private static final long IMAGE_CACHE_SIZE = 10 * 1024 * 1024;
 	private static final String CONTENT_CACHE_DIR = "content";
 	private static final long CONTENT_CACHE_SIZE = 5 * 1024 * 1024;
-	private DiskLruCache mImageDiskLruCache = null;
 	private DiskLruCache mContentDiskLruCache = null;
 
 	public void init() {
@@ -65,21 +59,6 @@ public class Config extends AbsConfig {
 			e.printStackTrace();
 		}
 		return 1;
-	}
-
-	public DiskLruCache getImageDiskCache(Context context) {
-		if (mImageDiskLruCache == null) {
-			try {
-				File cacheDir = getDiskCacheDir(context, IMAGE_CACHE_DIR);
-				if (!cacheDir.exists()) {
-					cacheDir.mkdirs();
-				}
-				mImageDiskLruCache = DiskLruCache.open(cacheDir, getAppVersion(context), 1, IMAGE_CACHE_SIZE);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return mImageDiskLruCache;
 	}
 
 	public DiskLruCache getContentDiskCache(Context context) {
@@ -121,72 +100,7 @@ public class Config extends AbsConfig {
 		return sb.toString();
 	}
 
-	public void writeImageCache() {
-		// new Thread(new Runnable() {
-		// @Override
-		// public void run() {
-		// try {
-		// String imageUrl =
-		// "http://img.my.csdn.net/uploads/201309/01/1378037235_7476.jpg";
-		// String key = hashKeyForDisk(imageUrl);
-		// DiskLruCache.Editor editor = mImageDiskLruCache.edit(key);
-		// if (editor != null) {
-		// OutputStream outputStream = editor.newOutputStream(0);
-		// if (downloadUrlToStream(imageUrl, outputStream)) {
-		// editor.commit();
-		// } else {
-		// editor.abort();
-		// }
-		// }
-		// mImageDiskLruCache.flush();
-		// } catch (IOException e) {
-		// e.printStackTrace();
-		// }
-		// }
-		// }).start();
-	}
-
-	public void readImageCache() {
-		try {
-			String imageUrl = "http://img.my.csdn.net/uploads/201309/01/1378037235_7476.jpg";
-			String key = hashKeyForDisk(imageUrl);
-			DiskLruCache.Snapshot snapShot = mImageDiskLruCache.get(key);
-			if (snapShot != null) {
-				InputStream is = snapShot.getInputStream(0);
-				Bitmap bitmap = BitmapFactory.decodeStream(is);
-				// mImage.setImageBitmap(bitmap);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void removeImageCache() {
-		try {
-			String imageUrl = "http://img.my.csdn.net/uploads/201309/01/1378037235_7476.jpg";
-			String key = hashKeyForDisk(imageUrl);
-			mImageDiskLruCache.remove(key);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public long getImageCacheSize() {
-		if (mImageDiskLruCache == null)
-			return 0;
-		return mImageDiskLruCache.size();
-	}
-
-	public void deleteImageCache() {
-		if (mImageDiskLruCache == null)
-			return;
-		try {
-			mImageDiskLruCache.delete();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+	
 
 	public void writeContentCache(final String uniqu, final String json) {
 		if(json==null||json.equals("")){
