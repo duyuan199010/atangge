4d5f7340d411bd1a43b4bd727edc1d5fc3a88f3c
laiying <laiying@9zhitx.com>
2015-10-15 11:31:48 +0800

    time format update

--- yssl/src/com/strod/yssl/util/DateUtil.java
+++ yssl/src/com/strod/yssl/util/DateUtil.java
@@ -1,11 +1,14 @@
 package com.strod.yssl.util;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtil {
-	public static String formatDateToString(long millis) {
+	public static String formatDateToString(long time) {
 		String interval = "";
+		//当前时间和time时间的间隔
+		long millis = System.currentTimeMillis() - time;
 		if (millis / 1000 < 10 && millis / 1000 >= 0) {
 			// 如果时间间隔小于10秒则显示“刚刚”millis/10得出的时间间隔的单位是秒
 			interval = "刚刚";
@@ -25,11 +28,33 @@ public class DateUtil {
 			int h = (int) (millis / 3600000);// 得出的时间间隔的单位是小时
 			interval = h + "小时前";
 
-		} else {
-			// 大于24小时，则显示正常的时间，但是不显示秒
+		} else if (isTimeInThisYear(time)){
+			// 大于24小时，在今年内，则显示正常的时间，但是不显示秒
 			SimpleDateFormat sdf = new SimpleDateFormat("MM-dd HH:mm");
-			interval = sdf.format(new Date(millis));
+			interval = sdf.format(new Date(time));
+		}else{
+			// 不在今年内，则显示正常的时间，但是不显示秒
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			interval = sdf.format(new Date(time));
 		}
 		return interval;
+	}
+	
+	private static boolean isTimeInThisYear(long millis){
+
+		Calendar c = Calendar.getInstance();
+		c.setTimeInMillis(millis);
+		int year = c.get(Calendar.YEAR);
+		if (year-getCurrentYear()==0){//在同一年
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	private static int getCurrentYear(){
+		Calendar c = Calendar.getInstance();
+		c.setTimeInMillis(System.currentTimeMillis());
+		return c.get(Calendar.YEAR);
 	}
 }
