5db2c5eaf76827d6432ee17ae336aec122306802
lying <lai_ying@163.com>
2015-12-11 20:02:30 +0800

    udpate

---
+++ yssl/src/com/strod/yssl/pages/search/SearchActivity.java
@@ -0,0 +1,11 @@
+package com.strod.yssl.pages.search;
+
+import com.roid.ui.AbsFragmentActivity;
+
+/**
+ * search
+ * Created by user on 15/12/2.
+ */
+public class SearchActivity extends AbsFragmentActivity{
+
+}
